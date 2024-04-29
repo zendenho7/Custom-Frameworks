76c48bf319b27b75ce6055f78766ff145ea8566a
hoshuhng <zendenho@gmail.com>
2024-04-29 22:12:37 +0800

    Temporary SplashScreen Implementation

--- Pathfinding-Algos/Pathfinding-Algos/Source/Menu/SplashScreen.cpp
+++ Pathfinding-Algos/Pathfinding-Algos/Source/Menu/SplashScreen.cpp
@@ -20,6 +20,8 @@ Copyright (c) 2024 Zen Ho
 
 namespace {
 	std::unique_ptr <Interface::RectButton> ssEntity;
+	std::unique_ptr <Drawables::D_Sprite> spriteEntity;
+	std::unique_ptr <Animation::SheetAnimator> sheetAnimator;
 }
 
 void SplashScreen::Load() {
@@ -33,11 +35,20 @@ void SplashScreen::Load() {
 }
 
 void SplashScreen::Init() {
+	//Button Init
 	ssEntity = std::make_unique<Interface::RectButton>(sf::Color::White, sf::Vector2f(250.0f, 50.0f), exEvents->windowCenter, 10.0f, 0.0f);
 	ssEntity->initButtonText("CLICK THIS BUTTON", exAssets->fonts["COMIC"], sf::Color::Black, {0.75f, 0.4f});
+
+	//Sprite Animation Init
+	spriteEntity = std::make_unique<Drawables::D_Sprite>(exAssets->textures["AME"], sf::IntRect(0, 0, 500, 500), sf::Vector2f(150.0f, 150.0f), exEvents->windowCenter + sf::Vector2f(0.0f, -100.0f));
+	sheetAnimator = std::make_unique<Animation::SheetAnimator>(sf::Vector2u( 2000, 2500 ), sf::Vector2i(500, 500), Animation::ANIMATE_SPEED, true, sf::Vector2u(0, 0), sf::Vector2u(2, 4));
+
 }
 
 void SplashScreen::Update() {
+
+	//Update Sprite Animation
+	sheetAnimator->animateTexture(*spriteEntity);
 
 	//Button To Enter Next Game State
 	if (ssEntity->isButtonClicked()) {
@@ -52,6 +63,9 @@ void SplashScreen::Draw() {
 
 	//Draw To Window
 	ssEntity->drawButton();
+
+	//Draw Sprite
+	exEvents->window.draw(*spriteEntity);
 }
 
 void SplashScreen::Free() {
